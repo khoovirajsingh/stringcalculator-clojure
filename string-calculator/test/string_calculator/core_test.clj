340973059dde869efb50c798d4610b58a0115e56
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-10-22 20:08:23 +0100

    project setup

---
+++ string-calculator/test/string_calculator/core_test.clj
@@ -0,0 +1,7 @@
+(ns string-calculator.core-test
+  (:require [clojure.test :refer :all]
+            [string-calculator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
